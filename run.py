74733bbb9fe073756796fef34fe10a5f3f830e65
Dimitri <dimitri.ray.boyce@gmail.com>
2024-08-05 18:20:09 -0400

    run.py edit

--- run.py
+++ run.py
@@ -1,1 +1,12 @@
+import os
+os.system("cls")
+print("welcome to the terminal tests project select menu")
+print("please select the project you want to run")
+print("")
+print("systest: when run if the system works it will print hello world")
+print("")
+user = input(">")
+
+if user == "systest":
+    os.system("python systest/main.py")
 
